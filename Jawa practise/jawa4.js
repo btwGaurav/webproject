a2361017f5b46df18be5ccc8122c740e7a03f201
btwGaurav <gauravsingh.gs.gs123@gmail.com>
2024-12-09 11:38:29 +0530

    practice

--- Jawa practise/jawa4.js
+++ Jawa practise/jawa4.js
@@ -1,5 +1,10 @@
-let a = document.querySelector(".btn")
+// let a = document.querySelector(".btn")
 
-a.addEventListener('click', function(){
-alert("button was clicked")
-})
+// a.addEventListener('click', function(){
+// alert("button was clicked")
+// })
+let signal= () => {
+    alert("Hello world is printed in console")
+}
+let b = console.log("hello world" , signal())
+
