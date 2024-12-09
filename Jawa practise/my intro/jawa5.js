6ab489e2e7bc94cba11bdfb87c929fa6c381d38a
btwGaurav <gauravsingh.gs.gs123@gmail.com>
2024-12-09 13:28:39 +0530

    made my intro

---
+++ Jawa practise/my intro/jawa5.js
@@ -0,0 +1,6 @@
+setTimeout(function(){document.querySelector(".second").innerHTML = ("I am from INDIA")}, 2000);
+setTimeout(function(){document.querySelector(".third").innerHTML = ("I love to play badminton")}, 4000);
+setTimeout(function(){document.querySelector(".fourth").innerHTML = ("I am currently learning jawa script")}, 6000);
+setTimeout(function(){document.querySelector(".fifth").innerHTML = ("I am 20 years old")}, 8000);
+setTimeout(function(){document.querySelector(".sixth").innerHTML = ("Always ready to work together")}, 10000);
+
